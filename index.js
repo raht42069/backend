be803b18b9420be93668cd2d0127b84ac02676ef
Vansh Maurya <vanshmaurya2003@gmail.com>
2024-02-28 18:31:41 +0530

    Add existing project files prior to the push to GitHub.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 import con from "./database.js";
 import bodyParser from "body-parser";
 import morgan from "morgan";
@@ -13,12 +13,31 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.static('client'));
 
+// Function to ping the server
+function pingServer() {
+    console.log('Pinging server...');
+    // Add your ping logic here, for example:
+    // You can ping the root endpoint of your server itself
+    fetch('https://blacklightvanshbackend.onrender.com')
+        .then(response => {
+            console.log('Server pinged successfully');
+        })
+        .catch(error => {
+            console.log('Error pinging server:', error);
+        });
+}
+
+// Ping the server immediately when it starts
+pingServer();
+
+// Schedule ping to run every 5 minutes (300,000 milliseconds)
+setInterval(pingServer, 300000);
+
 app.get("/weeklyLeaderBoard", async (req, res) => {
     try {
         console.log("weekl");
         const currentDate = new Date();
         const data = await con.query(`SELECT * FROM scoreData WHERE DATEDIFF(Date('${currentDate.toISOString()}'), Date(timeStamp)) < 7 order by score DESC LIMIT 200`)
-        //console.log(data[0]);
         res.send(data[0]);
     }
     catch (err) {
@@ -26,14 +45,12 @@ app.get("/weeklyLeaderBoard", async (req, res) => {
         res.send(err);
     }
 })
-
 
 app.post("/leaderboardByCountry", async (req, res) => {
     console.log(req.body.CountryCode);
     try {
         const currentDate = new Date();
         const data = await con.query(`SELECT * FROM scoreData WHERE DATEDIFF(Date('${currentDate.toISOString()}'), Date(timeStamp)) >= 7 AND DATEDIFF(Date('${currentDate.toISOString()}'), Date(timeStamp)) < 14 AND CountryCode = "${req.body.CountryCode}" ORDER BY score DESC LIMIT 200`)
-        console.log(data[0]);
         res.send(data[0]);
     }
     catch (err) {
@@ -42,13 +59,9 @@ app.post("/leaderboardByCountry", async (req, res) => {
     }
 })
 
-
-
 app.post("/rank", async (req, res) => {
     try{
         const data = await con.query(`SELECT COUNT(*) + 1 AS user_rank FROM scoreData WHERE Score > (SELECT Score FROM scoreData WHERE UID = ${req.body.UID})`)
-        
-        
         res.send(data[0]);
     }
     catch(err){
@@ -57,12 +70,10 @@ app.post("/rank", async (req, res) => {
     }
 })
 
-
-
-
 app.listen(PORT, (err) => {
     if (!err) {
         console.log(`server running at ${PORT}`);
+    } else {
+        console.log(err);
     }
-    else console.log(err);
 })
